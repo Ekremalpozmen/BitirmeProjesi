76dce73cba71015e1b298749e390ea9fa8e5a81e
Ekrem Alp Özmen <ekrem3792@gmail.com>
2022-04-01 18:06:04 +0300

    Veritabanı bağlantısı tamamlandı

--- BitirmeProjesi/Controllers/SecurityController.cs
+++ BitirmeProjesi/Controllers/SecurityController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BitirmeProjesi.ViewModels.User.Security;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,6 +11,24 @@ namespace BitirmeProjesi.Controllers
     {
         public ActionResult Login()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel login)
+        {
+            if (ModelState.IsValid)
+            {
+                if (login.UserName == "admin" && login.Password == "123")
+                {
+                    Session["UserName"] = login.UserName;
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Kullanıcı adı veya şifre hatalı");
+                }
+            }
             return View();
         }
 
