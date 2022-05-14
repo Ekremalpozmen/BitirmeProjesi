f6ee5751211e3f552b61f43bd00f10ff8460d9b7
Ekrem Alp Özmen <ekrem3792@gmail.com>
2022-05-14 22:23:54 +0300

    hayvan ekleme tamamlandı

--- BitirmeProjesi.Services/User/AnimalsService.cs
+++ BitirmeProjesi.Services/User/AnimalsService.cs
@@ -6,6 +6,7 @@ using LinqKit;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data.Entity;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Linq.Expressions;
@@ -23,7 +24,7 @@ namespace BitirmeProjesi.Services.User
         }
 
 
-        private IQueryable<AnimalListViewModel> _getAnimalListIQueryable(Expression<Func<Data.Animals, bool>> expr)
+        private IQueryable<AnimalListViewModel> _getAnimalListIQueryable(Expression<Func<Data.Animals, bool>> expr, UserModel user)
         {
             return (from a in _context.Animals.AsExpandable().Where(expr)
                     select new AnimalListViewModel()
@@ -46,11 +47,19 @@ namespace BitirmeProjesi.Services.User
                 predicate.And(a => a.Name.Contains(animalSearchViewModel.Name));
             }
 
-            return _getAnimalListIQueryable(predicate);
+            return _getAnimalListIQueryable(predicate, user);
+        }
+
+        public async Task<AnimalListViewModel> GetAnimalListViewAsync(int animalId, UserModel user)
+        {
+            var predicate = PredicateBuilder.New<Animals>(true);/*AND*/
+            predicate.And(a => a.Id == animalId);
+            var product = await _getAnimalListIQueryable(predicate, user).SingleOrDefaultAsync().ConfigureAwait(false);
+            return product;
         }
 
 
-        //public List<AnimalsViewModel> AnimalsList(AnimalsViewModel model, UserModel user)
+        //public List<AnimalListViewModel> AnimalsList(AnimalListViewModel model, UserModel user)
         //{
         //    using (var db = new SqlConnection(ConfigurationManager.ConnectionStrings["BitirmeProjesiConnectionString"].ConnectionString))
         //    {
@@ -62,45 +71,43 @@ namespace BitirmeProjesi.Services.User
         //                              ,[Gender]
         //                              ,[UserId]
         //                          FROM [BitirmeProjesi].[dbo].[Animals] where UserId=@userId";
-        //        var animalsList = (db.Query<AnimalsViewModel>(_sql, new { userId = user.Id })).ToList();
+        //        var animalsList = (db.Query<AnimalListViewModel>(_sql, new { userId = user.Id })).ToList();
         //        return animalsList;
         //    }
         //}
 
-        //public async Task<ServiceCallResult> AddAnimalsAsync(AnimalsViewModel model, UserModel user)
-        //{
-        //    var callResult = new ServiceCallResult() { Success = false };
+        public async Task<ServiceCallResult> AddAnimalsAsync(AnimalListViewModel model, UserModel user)
+        {
+            var callResult = new ServiceCallResult() { Success = false };
 
-        //    var animal = new Animals()
-        //    {
-        //        Name = model.Name,
-        //        Type = model.Type,
-        //        Birthdate = model.Birthdate,
-        //        Gender = model.Gender,
-        //        UserId = user.Id,
-        //    };
+            var animal = new Animals()
+            {
+                Name = model.Name,
+                Type = model.Type,
+                Birthdate = model.Birthdate,
+                Gender = model.Gender,
+                UserId = user.Id,
+            };
 
-        //    _context.Animals.Add(animal);
+            _context.Animals.Add(animal);
 
-        //    using (var dbTransaction = _context.Database.BeginTransaction())
-        //    {
-        //        try
-        //        {
-        //            await _context.SaveChangesAsync().ConfigureAwait(false);
-        //            dbTransaction.Commit();
-        //            callResult.Success = true;
-        //            callResult.Item = animal.Id;
-        //            callResult.SuccessMessages.Add("Hayvan Başarılı Şekilde Eklendi");
-        //            return callResult;
-        //        }
-        //        catch (Exception exc)
-        //        {
-        //            callResult.ErrorMessages.Add(exc.GetBaseException().Message);
-        //            return callResult;
-        //        }
-        //    }
-        //}
-
-
+            using (var dbTransaction = _context.Database.BeginTransaction())
+            {
+                try
+                {
+                    await _context.SaveChangesAsync().ConfigureAwait(false);
+                    dbTransaction.Commit();
+                    callResult.Success = true;
+                    callResult.Item = animal.Id;
+                    callResult.SuccessMessages.Add("Hayvan Başarılı Şekilde Eklendi");
+                    return callResult;
+                }
+                catch (Exception exc)
+                {
+                    callResult.ErrorMessages.Add(exc.GetBaseException().Message);
+                    return callResult;
+                }
+            }
+        }
     }
 }
--- BitirmeProjesi/Controllers/AnimalsController.cs
+++ BitirmeProjesi/Controllers/AnimalsController.cs
@@ -46,50 +46,48 @@ namespace BitirmeProjesi.Controllers
             };
         }
 
-
-
-
         public ActionResult AddAnimal()
         {
             return PartialView("~/Views/Animals/_AddAnimal.cshtml");
         }
 
-        //[HttpPost]
-        //public async Task<ActionResult> AddAnimal(AnimalListViewModel model)
-        //{
+        [HttpPost]
+        public async Task<ActionResult> AddAnimal(AnimalListViewModel model)
+        {
 
-        //    var callResult = await _animalsService.AddAnimalsAsync(model, CurrentUser);
-        //    //if (callResult.Success)
-        //    //{
+            var callResult = await _animalsService.AddAnimalsAsync(model, CurrentUser);
+            if (callResult.Success)
+            {
 
-        //    //    ModelState.Clear();
-        //    //    var animalId = (int)callResult.Item;
-        //    //    var viewModel =   _animalsService.AnimalsList(animalId, CurrentUser).ConfigureAwait(false);
-        //    //    var jsonResult = Json(
-        //    //        new
-        //    //        {
-        //    //            success = true,
-        //    //            warningMessages = callResult.WarningMessages,
-        //    //            successMessages = callResult.SuccessMessages,
-        //    //            responseText = RenderPartialViewToString("~/Views/Animals/Index.cshtml", viewModel),
-        //    //            item = viewModel
-        //    //        });
-        //    //    jsonResult.MaxJsonLength = int.MaxValue;
-        //    //    return jsonResult;
-        //    //}
-        //    //foreach (var error in callResult.ErrorMessages)
-        //    //{
-        //    //    ModelState.AddModelError("", error);
-        //    //}
-        //    return Json(
-        //        new
-        //        {
-        //            success = false,
-        //            errorMessages = callResult.ErrorMessages,
-        //            responseText = RenderPartialViewToString("~/Views/Animals/Index.cshtml", model)
-        //        });
+                ModelState.Clear();
+                var animalId = (int)callResult.Item;
+                var viewModel = _animalsService.GetAnimalListViewAsync(animalId, CurrentUser).ConfigureAwait(false);
 
-        //}
+                var jsonResult = Json(
+                    new
+                    {
+                        success = true,
+                        warningMessages = callResult.WarningMessages,
+                        successMessages = callResult.SuccessMessages,
+                        responseText = RenderPartialViewToString("~/Views/Animals/DisplayTemplates/AnimalListViewModel.cshtml", viewModel),
+                        item = viewModel
+                    });
+                jsonResult.MaxJsonLength = int.MaxValue;
+                return jsonResult;
+            }
+            foreach (var error in callResult.ErrorMessages)
+            {
+                ModelState.AddModelError("", error);
+            }
+            return Json(
+                new
+                {
+                    success = false,
+                    errorMessages = callResult.ErrorMessages,
+                    responseText = RenderPartialViewToString("~/Views/Animals/_AddAnimal.cshtml", model)
+                });
+
+        }
 
     }
 }
