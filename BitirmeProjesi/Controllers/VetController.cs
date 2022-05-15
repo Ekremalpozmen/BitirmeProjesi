c6698fa3f39bd845b08525ee051e189902bd5a03
Ekrem Alp Özmen <ekrem3792@gmail.com>
2022-05-15 15:39:52 +0300

    Kullanıcı Veterinerler Listesi Sayfa Tasarımı Tamamlandı #23

---
+++ BitirmeProjesi/Controllers/VetController.cs
@@ -0,0 +1,18 @@
+﻿using BitirmeProjesi.Controllers.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BitirmeProjesi.Controllers
+{
+    public class VetController : BaseController
+    {
+        // GET: Vet
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
