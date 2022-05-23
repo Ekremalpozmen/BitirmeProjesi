689a3befbb3c6dbd110156dd1b510f32fc18dd12
Ekrem Alp Özmen <ekrem3792@gmail.com>
2022-05-23 16:01:07 +0300

    Veteriner Kayıt Ol Ekran Backend Tamamlandı #36

--- BitirmeProjesi.Services/Vet/SecurityService.cs
+++ BitirmeProjesi.Services/Vet/SecurityService.cs
@@ -1,7 +1,9 @@
 ﻿using BitirmeProjesi.Data;
+using BitirmeProjesi.ViewModels.Common;
 using BitirmeProjesi.ViewModels.User.Security;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,42 +23,42 @@ namespace BitirmeProjesi.Services.Vet
             return _context.VetUsers.FirstOrDefault(x => x.UserName == login.UserName && x.Password == login.Password);
         }
 
-        //    public async Task<ServiceCallResult> Register(RegisterViewModel model)
-        //    {
-        //        var callResult = new ServiceCallResult() { Success = false };
-        //        bool userEmail = await _context.Users.AnyAsync(x => x.Email == model.Email || x.UserName == model.UserName);
+        public async Task<ServiceCallResult> Register(RegisterViewModel model)
+        {
+            var callResult = new ServiceCallResult() { Success = false };
+            bool userEmail = await _context.VetUsers.AnyAsync(x => x.Email == model.Email || x.UserName == model.UserName);
 
-        //        if (userEmail)
-        //        {
-        //            callResult.ErrorMessages.Add("E-mail veya Kullanıcı Adı kullanımda");
-        //            return callResult;
-        //        }
+            if (userEmail)
+            {
+                callResult.ErrorMessages.Add("E-mail veya Kullanıcı Adı kullanımda");
+                return callResult;
+            }
 
-        //        var user = new Users()
-        //        {
-        //            UserName = model.UserName,
-        //            Email = model.Email,
-        //            Password = model.Password,
-        //        };
-        //        _context.Users.Add(user);
-        //        using (var dbTransaction = _context.Database.BeginTransaction())
-        //        {
-        //            try
-        //            {
-        //                await _context.SaveChangesAsync().ConfigureAwait(false);
-        //                dbTransaction.Commit();
-        //                callResult.SuccessMessages.Add("Kullanıcı Kayıt Edildi");
-        //                callResult.Success = true;
-        //                return callResult;
-        //            }
-        //            catch (Exception exc)
-        //            {
-        //                callResult.ErrorMessages.Add(exc.GetBaseException().Message);
-        //                return callResult;
-        //            }
-        //        }
+            var vetuser = new VetUsers()
+            {
+                UserName = model.UserName,
+                Email = model.Email,
+                Password = model.Password,
+            };
+            _context.VetUsers.Add(vetuser);
+            using (var dbTransaction = _context.Database.BeginTransaction())
+            {
+                try
+                {
+                    await _context.SaveChangesAsync().ConfigureAwait(false);
+                    dbTransaction.Commit();
+                    callResult.SuccessMessages.Add("Kullanıcı Kayıt Edildi");
+                    callResult.Success = true;
+                    return callResult;
+                }
+                catch (Exception exc)
+                {
+                    callResult.ErrorMessages.Add(exc.GetBaseException().Message);
+                    return callResult;
+                }
+            }
 
-        //    }
-        //
+        }
+
     }
 }
--- BitirmeProjesi/Areas/Vet/Controllers/SecurityController.cs
+++ BitirmeProjesi/Areas/Vet/Controllers/SecurityController.cs
@@ -4,6 +4,7 @@ using BitirmeProjesi.ViewModels.User.Security;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
@@ -46,6 +47,32 @@ namespace BitirmeProjesi.Areas.Vet.Controllers
         public PartialViewResult Register()
         {
             return PartialView();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<ActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var registerResult = await _securityService.Register(model);
+                if (registerResult.Success)
+                {
+                    ModelState.Clear();
+                    foreach (var success in registerResult.SuccessMessages)
+                    {
+                        ModelState.AddModelError("", success);
+                        ViewBag.successRegister = success;
+                    }
+                    return View();
+                }
+                foreach (var error in registerResult.ErrorMessages)
+                {
+                    ModelState.AddModelError("", error);
+                    ViewBag.errorRegister = error;
+                }
+            }
+            return View();
         }
     }
 }
