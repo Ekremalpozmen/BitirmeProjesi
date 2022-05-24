1978367fc9ee6fe2fca4757864af766a31c136e7
Ekrem Alp Özmen <ekrem3792@gmail.com>
2022-05-24 18:57:21 +0300

    Veteriner Veterinerler Listesi Ekran Tasarımı Tamamlandı #41

---
+++ BitirmeProjesi/Areas/Vet/Controllers/VetController.cs
@@ -0,0 +1,32 @@
+﻿using BitirmeProjesi.Areas.Vet.Controllers.Abstract;
+using BitirmeProjesi.Services.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BitirmeProjesi.Areas.Vet.Controllers
+{
+    public class VetController : VetBaseController
+    {
+        private readonly VetService _vetService;
+        public VetController(VetService vetService)
+        {
+            _vetService = vetService;
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            //var model = await _vetService.GetVetListViewModel();
+            return View();
+        }
+
+        //public ActionResult VetProfile(int vetId)
+        //{
+        //    var model = _vetService.GetVetProfile(vetId);
+        //    return View(model);
+        //}
+    }
+}
