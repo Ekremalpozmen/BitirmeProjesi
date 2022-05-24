596f6eb56918873ff260b162da3ee84be8f25756
Ekrem Alp Özmen <ekrem3792@gmail.com>
2022-05-24 19:12:59 +0300

    Veteriner Profil Sayfası Ekran Tasarımı Tamamlandı #45

---
+++ BitirmeProjesi.Services/Vet/ProfileService.cs
@@ -0,0 +1,33 @@
+﻿using BitirmeProjesi.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitirmeProjesi.Services.Vet
+{
+    public class ProfileService
+    {
+        private readonly BitirmeProjesiEntities _context;
+        public ProfileService(BitirmeProjesiEntities context)
+        {
+            _context = context;
+        }
+        //public UserModel GetProfile(int userId)
+        //{
+        //    var userProfile = _context.Users.FirstOrDefault(x => x.Id == userId);
+        //    var profile = new UserModel()
+        //    {
+        //        Id = userId,
+        //        Name = userProfile.Name,
+        //        SurName = userProfile.SurName,
+        //        Email = userProfile.Email,
+        //        Gender = (bool)userProfile.Gender,
+        //        UserName = userProfile.UserName,
+        //    };
+
+        //    return profile;
+        //}
+    }
+}
---
+++ BitirmeProjesi/Areas/Vet/Controllers/ProfileController.cs
@@ -0,0 +1,22 @@
+﻿using BitirmeProjesi.Areas.Vet.Controllers.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BitirmeProjesi.Areas.Vet.Controllers
+{
+    public class ProfileController : VetBaseController
+    {
+        //private readonly ProfileService _profileService;
+        //public ProfileController(ProfileService profileService)
+        //{
+        //    _profileService = profileService;
+        //}
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
