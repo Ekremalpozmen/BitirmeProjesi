f268413c02c89c6d59c9a8b0968210bb26123a64
Ekrem Alp Özmen <ekrem3792@gmail.com>
2022-06-03 01:19:18 +0300

    Hayvan ekleme düzenlendi

--- BitirmeProjesi/Controllers/AnimalsController.cs
+++ BitirmeProjesi/Controllers/AnimalsController.cs
@@ -25,69 +25,23 @@ namespace BitirmeProjesi.Controllers
             return View(result);
         }
 
-        //public ActionResult AnimalList(AnimalSearchViewModel searchViewModel, int? page)
-        //{
-        //    var currentPageIndex = page - 1 ?? 0;
-
-        //    var result = _animalsService.GetAnimalListIQueryable(searchViewModel, CurrentUser)
-        //        .OrderBy(p => p.Name)
-        //        .ToPagedList(currentPageIndex, 10);
-
-        //    ModelState.Clear();
-        //    return new ContentResult
-        //    {
-        //        ContentType = "application/json",
-        //        Content = new JavaScriptSerializer { MaxJsonLength = Int32.MaxValue }.Serialize(new
-        //        {
-        //            success = true,
-        //            responseText = RenderPartialViewToString("~/Views/Animals/AnimalList.cshtml", result)
-        //        })
-        //    };
-        //}
-
         public ActionResult AddAnimal()
         {
             return PartialView("~/Views/Animals/_AddAnimal.cshtml");
         }
 
-        //[HttpPost]
-        //public async Task<ActionResult> AddAnimal(AnimalsViewModel model)
-        //{
-
-        //    var callResult = await _animalsService.AddAnimalsAsync(model, CurrentUser);
-        //    if (callResult.Success)
-        //    {
-
-        //        ModelState.Clear();
-        //        var animalIds = callResult.Item;
-        //        var animalId = Convert.ToInt32(animalIds);
-        //        var viewModel = _animalsService.GetAnimalListViewAsync(animalId, CurrentUser).ConfigureAwait(false);
-
-        //        var jsonResult = Json(
-        //            new
-        //            {
-        //                success = true,
-        //                warningMessages = callResult.WarningMessages,
-        //                successMessages = callResult.SuccessMessages,
-        //                responseText = RenderPartialViewToString("~/Views/Animals/DisplayTemplates/AnimalListViewModel.cshtml", viewModel),
-        //                item = viewModel
-        //            });
-        //        jsonResult.MaxJsonLength = int.MaxValue;
-        //        return jsonResult;
-        //    }
-        //    foreach (var error in callResult.ErrorMessages)
-        //    {
-        //        ModelState.AddModelError("", error);
-        //    }
-        //    return Json(
-        //        new
-        //        {
-        //            success = false,
-        //            errorMessages = callResult.ErrorMessages,
-        //            responseText = RenderPartialViewToString("~/Views/Animals/_AddAnimal.cshtml", model)
-        //        });
-
-        //}
+        [HttpPost]
+        public async Task<ActionResult> AddAnimal(AnimalsViewModel model)
+        {
+            var callResult = await _animalsService.AddAnimalsAsync(model, CurrentUser);
+            return Json(
+                new
+                {
+                    success = false,
+                    errorMessages = callResult.ErrorMessages,
+                    responseText = RenderPartialViewToString("~/Views/Animals/Index.cshtml", model)
+                });
+        }
 
 
         public async Task<ActionResult> VaccineList(int animalId)
