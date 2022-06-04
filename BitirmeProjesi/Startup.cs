64afa392f05aeb732c46a35d56509d02406439e4
Ekrem Alp Özmen <ekrem3792@gmail.com>
2022-06-04 15:42:31 +0300

    SignalR kütüphanesi kurulumu tamamlandı.

---
+++ BitirmeProjesi/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(BitirmeProjesi.Startup))]
+
+namespace BitirmeProjesi
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder App)
+        {
+            App.MapSignalR();
+        }
+    }
+}
