77010b1b729c78ae1a7d4951ed4db67fa97544aa
Ekrem Alp Özmen <ekrem3792@gmail.com>
2022-06-12 18:33:02 +0300

    Veteriner Sorular Detay Görme Tasarımı (Modal ile yapılacak) Tamamlandı  #39

---
+++ BitirmeProjesi/Areas/Vet/Controllers/MessageController.cs
@@ -0,0 +1,27 @@
+﻿using BitirmeProjesi.Areas.Vet.Controllers.Abstract;
+using BitirmeProjesi.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BitirmeProjesi.Areas.Vet.Controllers
+{
+    public class MessageController : VetBaseController
+    {
+        BitirmeProjesiEntities db = new BitirmeProjesiEntities();
+
+        public ActionResult VetMessagesDetail(int questionId)
+        {
+            List<FromUserToVetMessages> userToVetMessage = db.FromUserToVetMessages.Where(x => x.QuestionsId == questionId).ToList();
+            ViewBag.fromUserToVetMessages = userToVetMessage;
+
+            List<FromVetToUserMessages> vetToUserMessage = db.FromVetToUserMessages.Where(x => x.QuestionsId == questionId).ToList();
+            ViewBag.fromVetToUserMessages = vetToUserMessage;
+
+            return PartialView("~/Areas/Vet/Views/Message/_VetMessagesDetail.cshtml");
+        }
+    }
+}
