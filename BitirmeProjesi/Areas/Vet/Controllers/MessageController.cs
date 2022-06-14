4f09c7170c434a63c453ff92b94b9e1172f35a80
Ekrem Alp Özmen <ekrem3792@gmail.com>
2022-06-14 19:56:19 +0300

    Veterinerden kullanıcıya giden mesaj veritabanına kaydet Tamamlandı  #55

--- BitirmeProjesi/Areas/Vet/Controllers/MessageController.cs
+++ BitirmeProjesi/Areas/Vet/Controllers/MessageController.cs
@@ -18,10 +18,34 @@ namespace BitirmeProjesi.Areas.Vet.Controllers
             List<FromUserToVetMessages> userToVetMessage = db.FromUserToVetMessages.Where(x => x.QuestionsId == questionId).ToList();
             ViewBag.fromUserToVetMessages = userToVetMessage;
 
+            ViewBag.userId = userToVetMessage.Select(x => x.FromUserId).FirstOrDefault();
+            ViewBag.vetId = userToVetMessage.Select(x => x.ToVetId).FirstOrDefault();
+            ViewBag.questionId = questionId;
+
+
+
             List<FromVetToUserMessages> vetToUserMessage = db.FromVetToUserMessages.Where(x => x.QuestionsId == questionId).ToList();
             ViewBag.fromVetToUserMessages = vetToUserMessage;
 
             return PartialView("~/Areas/Vet/Views/Message/_VetMessagesDetail.cshtml");
+        }
+
+        [HttpPost]
+        public ActionResult VetSendMessage(string contentMessage, int userId, int vetId, int questionId)
+        {
+            var message = new FromVetToUserMessages()
+            {
+                ContentMessage = contentMessage,
+                CreateDate = DateTime.Now,
+                FromVetId = vetId,
+                ToUserId = userId,
+                QuestionsId = questionId,
+                Status = 2
+            };
+
+            db.FromVetToUserMessages.Add(message);
+            db.SaveChanges();
+            return View();
         }
     }
 }
